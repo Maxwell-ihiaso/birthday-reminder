2b56c51e0d21e60c5316f44632b309f5763448de
Maxwell Ihiaso <ihiasomaxwellchukwuebuka@gmail.com>
2021-11-22 21:08:36 +0100

    added alt text to image

--- src/List.js
+++ src/List.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 const List = (props) => {
-  const { id, name, age, image } = props.data;
+  const { name, age, image } = props.data;
+  // console.log(image.split("/")[image.split("/").length - 1]);
 
   return (
     <div className="person">
-      <img src={image} alt="" />
+      <img src={image} alt={image.split("/")[image.split("/").length - 1]} />
       <div>
         <h4>{name}</h4>
         <p>{age} years</p>
